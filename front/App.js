6da5b0764f571fc0840f5fc7f21b21542b8fb8e6
EddyZard <131426029+EddyZard@users.noreply.github.com>
2025-11-29 23:31:30 +0000

    Tentando conectar com Firebase part 2

--- front/App.js
+++ front/App.js
@@ -2,7 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Biblioteca from './Biblioteca';
 
+
+import firebase from '@react-native-firebase/app';
+
 export default function App() {
+
+  console.log(firebase.app())
+
   return (
     <View style={styles.container}>
       <Biblioteca />
